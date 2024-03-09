b907440153848771b62fdc4f03844e0cd518c5b5
namth1711 <trinhnam877@gmail.com>
2024-03-09 13:01:38 +0700

    Initial commit

---
+++ app/src/main/java/fpoly/namth/myapplication/FCMNotificationService.java
@@ -0,0 +1,6 @@
+package fpoly.namth.myapplication;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class FCMNotificationService extends FirebaseMessagingService {
+}
---
+++ app/src/main/java/fpoly/namth/myapplication/LoginActivity.java
@@ -0,0 +1,101 @@
+package fpoly.namth.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import fpoly.namth.myapplication.model.UserModel;
+import fpoly.namth.myapplication.utils.FirebaseUtil;
+
+public class LoginActivity extends AppCompatActivity {
+    Button btnLogin;
+    EditText edUsername, edPassword;
+    TextView loginPhoneNumber;
+    FirebaseAuth mAuth;
+    UserModel userModel;
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            finish();
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        edUsername = findViewById(R.id.login_uname);
+        edPassword = findViewById(R.id.login_password);
+        btnLogin = findViewById(R.id.btnlogin);
+        loginPhoneNumber = findViewById(R.id.tv_login_number_phone);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = edUsername.getText().toString();
+                String password = edPassword.getText().toString();
+
+                if (username.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Please enter email and password", Toast.LENGTH_SHORT).show();
+                } else {
+                    if(userModel!=null){
+                        userModel.setUsername(username);
+                    }else{
+                        userModel = new UserModel(username, Timestamp.now(), FirebaseUtil.currentUserId());
+                    }
+                    mAuth.signInWithEmailAndPassword(username, password)
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Log.d("login", "signInWithEmail:success");
+                                        Toast.makeText(LoginActivity.this, "Đăng Nhập Thành Công", Toast.LENGTH_SHORT).show();
+                                        FirebaseUtil.currentUserDetails().set(userModel).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if(task.isSuccessful()){
+                                                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                                                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK );
+                                                    startActivity(intent);
+                                                }
+                                            }
+                                        });
+                                    } else {
+                                        Log.w("login", "signInWithEmail:failure", task.getException());
+                                        Toast.makeText(LoginActivity.this, "Đăng Nhập Thất Bại", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+        loginPhoneNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,LoginPhoneNumberActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
